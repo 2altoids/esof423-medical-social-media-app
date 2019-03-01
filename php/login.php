40096fe2866ee8be46e6a9e68f18ccc781cce829
TheGreatFatCat <thefatcatproductons@gmail.com>
2019-03-01 08:30:13 -0700

    Small changes to login func

--- php/login.php
+++ php/login.php
@@ -1,65 +1,72 @@
 <?php
- 
-//Sample Database Connection Script 
-//Setup connection variables, such as database username
-//and password
-$hostname="localhost";
-$username="esofmsna_User";
-$password="Lemming17";
-$dbname="esofmsna_LoginDatabase";
-$usertable="LoginTable";
+
 //email and password (display name is set on register)
 $Pass = $_POST['pass'];
 $Email = $_POST['email'];
- 
-//Connect to the database
-$connection = mysqli_connect($hostname, $username, $password, $dbname);
-//mysql_select_db($dbname, $connection);
- if ($connection->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
 
 
+//method to connect to DB
+function connectDB()
+{
+    //Connect to the database (using hard coded values as the database wont change")
+    $conn = mysqli_connect("localhost", "esofmsna_User", "Lemming17", "esofmsna_LoginDatabase");
+    //mysql_select_db($dbname, $connection);
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+        
+    }
+    return $conn;
+}
 
+//function to connect to vb and verify entered credentials
+function login ($Pass, $Email)
+{
+    //setup global vars;
+    //global $Pass, $Email;
+    
+    //get our database connection
+    $conn = connectDB();
+    
     echo "Pre query";
     //create query
     $sql = "SELECT pass FROM LoginTable WHERE email='$Email'";
     //get result
-    $result = mysqli_query($connection, $sql);
-
+    $result = mysqli_query($conn, $sql);
+    
     if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-         while($row = $result->fetch_assoc())
-         {
+        // output data of each row
+        while($row = $result->fetch_assoc())
+        {
             $StoredPass = $row["pass"];
-           
-           if($StoredPass == $Pass)
+            
+            if($StoredPass == $Pass)
             {   echo "Password match";
-                //passwords are the same, push home page
-                header('Location: ../html/clinician.html'); //exit
-                 exit;
+            //passwords are the same, push home page
+            header('Location: ../html/clinician.html'); //exit
+            return true;
+            exit;
             }
-             else
+            else
             { //passwords do not match echo faliure and kick to index
-             echo "Password Mismatch";
-             header('Location: ../index.html'); //exit back to index.html
-              exit;
-            } 
-        
-        
-        
-        } 
+                echo "Password Mismatch";
+                header('Location: ../index.html'); //exit back to index.html
+                return false;
+                exit;
+            }
+        }
     }
     else //if no data echo failed login and re direct to index
     {
         echo "No password for that email account";
-       header('Location: ../index.html'); //exit back to index.html
-       exit;
+        header('Location: ../index.html'); //exit back to index.html
+        return false;
+        exit;
     }
     
-    
+    mysqli_close($conn);
+}
 
-mysqli_close($connection);
- 
+//call login
+login($Pass, $Email);
+
 ?>
-
