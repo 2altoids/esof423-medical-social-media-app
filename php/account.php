2fafdfdd3d0f53dfd6cc73f8a32b960e4f553667
really165 <31826004+really165@users.noreply.github.com>
2019-04-12 17:57:38 -0600

    added user and account php scripts

---
+++ php/account.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Account</title>
+	<link rel="stylesheet" href="../css/user.css" type="text/css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+* {
+  box-sizing: border-box;
+}
+	</style>
+</head>
+
+<body>
+
+<div class="header">
+  <h1>Account</h1>
+</div>
+
+<div class="navbar">
+	<a href="../php/user.php">Home</a>
+	<a href="#">Account</a>
+	<a href="../php/friends.php">Friends</a>
+	<input type="text" placeholder="Search..">
+</div>
+
+    
+ 
+  <div class="main">
+    <h2>Edit User Profile</h2>
+    <div class="container">
+		<form action="/action_page.php">
+			<label for="fname">First Name</label>
+			<input type="text" id="fname" name="firstname" placeholder="First Name">
+			
+			<label for="lname">Last Name</label>
+			<input type="text" id="lname" name="lastname" placeholder="Last Name">
+			
+			<label for="pnum">Phone Number</label>
+			<input type="text" id="pnum" name="phonenumber" placeholder="Phone Number">
+			
+			<label for="email">Email</label>
+			<input type="text" id="email" name="email" placeholder="Email">
+			
+			<label for="address">Address</label>
+			<input type="text" id="address" name="address" placeholder="Address">
+			
+			<label for="medHist">Medical History</label>
+			<textarea id="medHist" name="medHist" placeholder="Any medical history..." style="height:200px"></textarea>
+			
+			<label for="addInfo">Additional Information</label>
+			<textarea id="addInfo" name="addInfo" placeholder="Any additional information..." style="height:200px"></textarea>
+       
+			<input type="submit" value="Apply">
+		</form>
+   <br>
+    
+  </div>
+</div>
+
+
+<div class="footer">
+	Andrew Smith | Kyle Busch | James Tomasko | Kala Lightner<br>
+	ESOF 423 SocialMediaApp
+	<a href="https://github.com/2altoids/esof423-medical-social-media-app">Click for User Doumentation</a>
+</div>
+	
+</body>
+</html>
---
+++ php/user.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Homepage</title>
+	<link rel="stylesheet" href="../css/user.css" type="text/css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<style>
+* {
+  box-sizing: border-box;
+}
+	</style>
+</head>
+
+<body>
+
+<div class="header">
+  <h1>Welcome!</h1>
+  <p>Medical Social Connect</p>
+</div>
+
+<div class="navbar">
+  <a href="#">Home</a>
+  <a href="../php/account.php">Account</a>
+  <a href="../php/friends.php">Friends</a>
+  <input type="text" placeholder="Search..">
+</div>
+
+<div class="row">
+  <div class="side">
+    <h2>About Me</h2>
+    <h5>Photo of me:</h5>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>User Information</p>
+    
+  </div>
+  <div class="main">
+    <h2>NAME HERE</h2>
+    <h5>This is an area for future posting</h5>
+    <div class="fakeimg" style="height:200px;">Image</div>
+    <p>Some text..</p>
+   <br>
+    
+  </div>
+</div>
+
+
+<div class="footer">
+	Andrew Smith | Kyle Busch | James Tomasko | Kala Lightner<br>
+	ESOF 423 SocialMediaApp
+	<a href="https://github.com/2altoids/esof423-medical-social-media-app">Click for User Doumentation</a>
+</div>
+	
+</body>
+</html>
